313b6a7af5d5afed0a30d544cd9ba88705f0226d
Mark Richardson <mark.richardson4@gmail.com>
2021-02-09 16:14:35 +0000

    first commit, workspace set up

---
+++ src/main/java/com/example/codeclan/filefolderservice/FilefolderserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.filefolderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilefolderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilefolderserviceApplication.class, args);
+	}
+
+}
