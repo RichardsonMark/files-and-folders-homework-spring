c47c9fb8d0ace8f0bef2304d2f7616e5aea30949
Mark Richardson <mark.richardson4@gmail.com>
2021-02-09 20:06:17 +0000

    Fixed spelling error on UserRepository file name, added tests to make sure user, file and folder cnbe created

--- src/main/java/com/example/codeclan/filefolderservice/controllers/FileController.java
+++ src/main/java/com/example/codeclan/filefolderservice/controllers/FileController.java
@@ -1,4 +1,13 @@
 package com.example.codeclan.filefolderservice.controllers;
 
+import com.example.codeclan.filefolderservice.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FileController {
+
+    @Autowired
+    FileRepository fileRepository;
+
 }
--- src/test/java/com/example/codeclan/filefolderservice/FilefolderserviceApplicationTests.java
+++ src/test/java/com/example/codeclan/filefolderservice/FilefolderserviceApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.codeclan.filefolderservice;
 
+import com.example.codeclan.filefolderservice.models.File;
+import com.example.codeclan.filefolderservice.models.Folder;
+import com.example.codeclan.filefolderservice.models.User;
+import com.example.codeclan.filefolderservice.repositories.FileRepository;
+import com.example.codeclan.filefolderservice.repositories.FolderRepository;
+import com.example.codeclan.filefolderservice.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FilefolderserviceApplicationTests {
 
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	FileRepository fileRepository;
+	@Autowired
+	FolderRepository folderRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canAddNewUser() {
+		User user = new User("Mark");
+		userRepository.save(user);
+	}
+
+	@Test
+	public void canAddNewUserAndFolder() {
+		User user = new User("Mark2");
+		userRepository.save(user);
+		Folder folder = new Folder("Mark's Homework", user);
+		folderRepository.save(folder);
+	}
+
+	@Test
+	public void canAddNewUserFileAndFolder() {
+		User user = new User("Mark3");
+		userRepository.save(user);
+		Folder folder = new Folder("Mark's Classwork", user);
+		folderRepository.save(folder);
+		File file = new File("ClassNotes", "txt", 1024, folder);
+		fileRepository.save(file);
 	}
 
 }
