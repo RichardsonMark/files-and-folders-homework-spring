ad634f5fa0afb4810d04c3639b45b9f1b7bdee90
Mark Richardson <mark.richardson4@gmail.com>
2021-02-09 19:32:08 +0000

    Folder and User classes and file, folder and user repositories set up

--- src/main/java/com/example/codeclan/filefolderservice/repositories/UserRespository.java
+++ src/main/java/com/example/codeclan/filefolderservice/repositories/UserRespository.java
@@ -1,4 +1,9 @@
 package com.example.codeclan.filefolderservice.repositories;
 
-public interface UserRespository {
+import com.example.codeclan.filefolderservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRespository extends JpaRepository<User, Long> {
 }
